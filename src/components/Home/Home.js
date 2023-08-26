a62c3fd3404144e88bb9c903c8985dadc4ca9f0e
Fedor <chesnovski99@gmail.com>
2023-08-26 18:29:19 +0200

    Added priceChanged and changed mobile version design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,6 +13,7 @@ async function fetchCoins(skip ) {
 
   return data.coins
 }
+
   
 
 const Home = () => {
@@ -30,22 +31,30 @@ const Home = () => {
     return <h3> Error receiving data</h3>
   }
 
- 
 
   return (
   <>
-    <div class="ml-12 grid grid-flow-col gap-36">
+    <div class="mx-12 grid grid-flow-col gap-48">
         <div>
               {data ? 
-          <div>
+          <div >
             {data.map(coin => (
-              <div key={coin.id}>
-                <img className="rounded-full h-8 inline-block mr-2"
-                src={coin.icon}
-                alt="" />
-                {coin.name}
-                <p>{coin.price.toFixed(3)}$</p>
-                <hr/>
+              <div>
+                <div className='max-sm:flex max-sm:justify-normal max-sm:items-center' key={coin.id}>
+                  <div className='grid grid-cols-2 gap-4'>
+                    <div className='mx-6'>
+                      <div className='flex justify-normal items-center'>
+                        <img className="rounded-full h-8 mb-2 inline-block mr-2"
+                        src={coin.icon}
+                        alt="" />
+                        <p className='mx-2'>{coin.name}</p>
+                      </div>
+                      <p className='mx-1'>{coin.price.toFixed(3)}$</p>
+                  </div>
+                    <p className='mx-8 my-6'>{coin.priceChange1d}%</p>
+                  </div>
+                </div>
+                  <hr className='h-px my-2 bg-gray-200 border-0 dark:bg-gray-700'/>
               </div>
             ))}
           </div>
@@ -54,14 +63,14 @@ const Home = () => {
         </div>
 
 
-        <div className='invisible  md:visible flex items-center '>
+        <div className='hidden  md:block  md:items-center '>
           <img src="https://alternative.me/crypto/fear-and-greed-index.png" alt="Latest Crypto Fear & Greed Index" />
         </div>
 
       </div>  
     
 
-    <div className='ml-32 mt-6 '>
+    <div className='mx-8 mt-4  md:ml-32 md:mt-6 '>
       {page === 0 ? 
         <div className='flex justify-start'>
           <div className="btn-disabled  flex justify-center items-center" >
