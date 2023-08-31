afc199e81721eb262404e4175af6af158cc2bd95
Fedor <chesnovski99@gmail.com>
2023-08-31 21:47:55 +0200

    Added search field with redux

--- src/components/ui/Screens/Header/Header.tsx
+++ src/components/ui/Screens/Header/Header.tsx
@@ -1,29 +1,64 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addSearch } from "../../../../store/reducer/coinReducer";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
   const handleMenu = () => {
     setOpen((prev) => !prev);
-    console.log(open);
   };
+
+  const dispatch = useDispatch();
+  const [text, setText] = useState("");
+
+  const addCoinToSearch = (name: string) => {
+    dispatch(addSearch(name));
+  };
+
+  const handleOnClick = () => {
+    addCoinToSearch(text);
+    setText("");
+  };
+
   return (
     <>
       <div className="header flex ">
-        <div className="w-5/6 ">
+        <div className="w-2/3 py-1">
           <h1 className="ml-16 hidden md:block">
             Please be calm and take your profit
           </h1>
           <h1 className="mx-auto  md:hidden">Please take your profit</h1>
         </div>
 
-        <div className="w-1/6 flex align-center ">
-          <div className="hidden md:block">
-            <a className="mr-4" href="/">
-              Home
-            </a>
-            <a className="mr-4" href="/about">
-              About
-            </a>
+        <div className="w-1/3">
+          <div className="hidden md:flex md:justify-center md:content-end">
+            <div className="mx-4 py-1">
+              <input
+                className="mx-2 border rounded px-2"
+                type="text"
+                placeholder="Search Coin"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              />
+              <Link to="/searchcoin">
+                <button
+                  className="hover:bg-gray-600 hover:bg-opacity-20 rounded border hover:border-transparent px-2   text-black bg-transparent"
+                  disabled={!text}
+                  onClick={handleOnClick}
+                >
+                  Search
+                </button>
+              </Link>
+            </div>
+            <div className="px-2 py-1">
+              <a className="mr-4" href="/">
+                Home
+              </a>
+              <a className="mr-4" href="/about">
+                About
+              </a>
+            </div>
           </div>
           <div className="md:hidden">
             <div onClick={handleMenu} className="my-2 mx-2 space-y-2">
@@ -43,6 +78,26 @@ const Header = () => {
             <a className="ml-2" href="/about">
               About
             </a>
+          </div>
+          <div className="flex justify-start content-center my-4">
+            <div className="">
+              <input
+                className="mx-2"
+                type="text"
+                placeholder="Search Coin"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              />
+              <Link to="/searchcoin">
+                <button
+                  className="bg-gray-600 rounded border px-2 py-1 bg-opacity-20  text-black"
+                  disabled={!text}
+                  onClick={handleOnClick}
+                >
+                  Search
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       ) : null}
--- src/components/ui/Screens/SearchMenu/SearchMenu.tsx
+++ src/components/ui/Screens/SearchMenu/SearchMenu.tsx
@@ -4,9 +4,8 @@ import { useQuery } from "react-query";
 import styles from "../SearchMenu/SearchMenu.module.scss";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import { useDispatch, useSelector } from "react-redux";
-import { addSearch } from "../../../../store/reducer/coinReducer";
 import { RootState } from "../../../../store/store";
+import { useDispatch, useSelector } from "react-redux";
 
 async function fetchCoin(name: string) {
   const { data } = await axios.get(
@@ -17,14 +16,9 @@ async function fetchCoin(name: string) {
 }
 
 const SearchMenu: FC = () => {
-  const dispatch = useDispatch();
-  const coin = useSelector((state: RootState) => state.coin.coin);
+  const coinName = useSelector((state: RootState) => state.coin.coin);
 
-  const addCoinToSearch = (name: string) => {
-    dispatch(addSearch(name));
-  };
-
-  const [coinName, setCoinName] = useState("solana");
+  //   const [coinName, setCoinName] = useState("solana");
   const { data, isLoading, isError } = useQuery({
     queryKey: ["coin", coinName],
     queryFn: () => fetchCoin(coinName),
@@ -42,47 +36,47 @@ const SearchMenu: FC = () => {
   return (
     <div className={styles.parent}>
       <div>
-        <div className={styles.main} key={data.id}>
-          <div className="grid grid-cols-2 gap-4">
-            <div className="mx-6">
-              <div className="flex justify-normal items-center">
-                <img className={styles.coinImg} src={data.icon} alt="" />
-                <p className="mx-2">{data.name}</p>
+        {data ? (
+          <div className={styles.main} key={data.id}>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="mx-6">
+                <div className="flex justify-normal items-center">
+                  <img className={styles.coinImg} src={data.icon} alt="" />
+                  <p className="mx-2">{data.name}</p>
+                </div>
+                <p className="mx-1 font-mono text-lg">
+                  {data.price.toFixed(3)}$
+                </p>
               </div>
-              <p className="mx-1 font-mono text-lg">{data.price.toFixed(3)}$</p>
+              {data.priceChange1d ? (
+                <div
+                  className={` ${styles.priceChanged} ${
+                    data.priceChange1d > 4
+                      ? " bg-green-600 text-green-800   transition-opacity animate-ping-short hover:animate-none "
+                      : data.priceChange1d < -4
+                      ? "bg-rose-600 animate-ping-short text-red-800  hover:animate-none"
+                      : data.priceChange1d < 0
+                      ? "bg-rose-600 text-red-800"
+                      : "bg-green-600 text-green-800  "
+                  }`}
+                >
+                  {data.priceChange1d > 0 ? (
+                    <ArrowDropUpIcon />
+                  ) : (
+                    <ArrowDropDownIcon />
+                  )}
+                  {data.priceChange1d} %
+                </div>
+              ) : null}
             </div>
-            {data.priceChange1d ? (
-              <div
-                className={` ${styles.priceChanged} ${
-                  data.priceChange1d > 4
-                    ? " bg-green-600 text-green-800   transition-opacity animate-ping-short hover:animate-none "
-                    : data.priceChange1d < -4
-                    ? "bg-rose-600 animate-ping-short text-red-800  hover:animate-none"
-                    : data.priceChange1d < 0
-                    ? "bg-rose-600 text-red-800"
-                    : "bg-green-600 text-green-800  "
-                }`}
-              >
-                {data.priceChange1d > 0 ? (
-                  <ArrowDropUpIcon />
-                ) : (
-                  <ArrowDropDownIcon />
-                )}
-                {data.priceChange1d} %
-              </div>
-            ) : null}
           </div>
-        </div>
+        ) : (
+          <div className="text-base font-mono font-bold text-xl">
+            Please write entire name of coin
+          </div>
+        )}
+
         <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700" />
-      </div>
-      <div className=" w-48 h-10 flex justify-between align-center  mx-4 my-4 rounded-2 border-gray-600 border px-4 py-2">
-        <div className="mx-2">{coin}</div>
-        <button
-          className="mx-2"
-          onClick={() => addCoinToSearch(String(prompt()))}
-        >
-          Add
-        </button>
       </div>
     </div>
   );
