a20958359ca21199f12b0a0cbd30fe87b9e95cb0
Fedor <chesnovski99@gmail.com>
2023-08-31 17:42:46 +0200

    Added SearchCoin menu design

--- src/components/ui/Screens/SearchMenu/SearchMenu.tsx
+++ src/components/ui/Screens/SearchMenu/SearchMenu.tsx
@@ -2,11 +2,8 @@ import React, { FC, useState } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 import styles from "../Home/Home.module.scss";
-
-interface ICoin {
-  id: number;
-  name: string;
-}
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 async function fetchCoin(name: string) {
   const { data } = await axios.get(
@@ -17,7 +14,7 @@ async function fetchCoin(name: string) {
 }
 
 const SearchMenu: FC = () => {
-  const [coinName, setCoinName] = useState("bitcoin");
+  const [coinName, setCoinName] = useState("solana");
   const { data, isLoading, isError } = useQuery({
     queryKey: ["coin", coinName],
     queryFn: () => fetchCoin(coinName),
@@ -34,7 +31,40 @@ const SearchMenu: FC = () => {
 
   return (
     <div className={styles.parent}>
-      <div>{data ? <div>{data.name}</div> : <div> No data </div>}</div>
+      <div>
+        <div className={styles.main} key={data.id}>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="mx-6">
+              <div className="flex justify-normal items-center">
+                <img className={styles.coinImg} src={data.icon} alt="" />
+                <p className="mx-2">{data.name}</p>
+              </div>
+              <p className="mx-1 font-mono text-lg">{data.price.toFixed(3)}$</p>
+            </div>
+            {data.priceChange1d ? (
+              <div
+                className={` ${styles.priceChanged} ${
+                  data.priceChange1d > 4
+                    ? " bg-green-600 text-green-800   transition-opacity animate-ping-short hover:animate-none "
+                    : data.priceChange1d < -4
+                    ? "bg-rose-600 animate-ping-short text-red-800  hover:animate-none"
+                    : data.priceChange1d < 0
+                    ? "bg-rose-600 text-red-800"
+                    : "bg-green-600 text-green-800  "
+                }`}
+              >
+                {data.priceChange1d > 0 ? (
+                  <ArrowDropUpIcon />
+                ) : (
+                  <ArrowDropDownIcon />
+                )}
+                {data.priceChange1d} %
+              </div>
+            ) : null}
+          </div>
+        </div>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700" />
+      </div>
     </div>
   );
 };
